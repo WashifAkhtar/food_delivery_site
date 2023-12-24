96d1e6f8edc3fc76e9aab907e9005a484666c6e3
WashifAkhtar <washifakhtar123@gmal.com>
2023-12-24 22:17:26 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+
+const fileContent = fs.readFileSync('index.html');
+const hostname = '127.0.0.1';
+const port = 80;
+
+const server = http.createServer((req, res)=>{
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.end(fileContent);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+ 
